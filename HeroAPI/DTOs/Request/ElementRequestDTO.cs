c81407bcabf5a88ab8f56c6071b1b840a14d7a52
Revannn <rghofur053@gmail.com>
2024-03-05 14:23:46 +0700

    done

---
+++ HeroAPI/DTOs/Request/ElementRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using HeroAPI.Database;
+using System.Text.Json.Serialization;
+
+namespace HeroAPI.DTOs.Request
+{
+    public class ElementRequestDTO : Element
+    {
+        [JsonIgnore]
+        public int Id { get => base.Id; set => base.Id = value; }
+
+        [JsonIgnore]
+        public override ICollection<Skill> Skills { get => base.Skills; set => base.Skills = value; }
+    }
+}
---
+++ HeroAPI/DTOs/Response/ElementResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿using HeroAPI.Database;
+
+namespace HeroAPI.DTOs.Response
+{
+    public class ElementResponseDTO : Element
+    {
+        public override ICollection<Skill> Skills { set => base.Skills = value; }
+    }
+}
